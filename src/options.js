6a2538c6890c1921547d091ea255f9c38863f468
sanjaymusale <sanjay@codebrahma.com>
2019-10-22 11:08:27 +0530

    initial multi level selector implementation

---
+++ src/options.js
@@ -0,0 +1,70 @@
+const options = [
+  {
+    value: 'department',
+    label: 'Department',
+    options: [
+      {
+        value: 'customer success',
+        label: 'Customer success',
+      },
+      {
+        value: 'marketing',
+        label: 'Marketing',
+      },
+      {
+        value: 'operations',
+        label: 'Operations',
+      },
+    ],
+  },
+  {
+    value: 'office location',
+    label: 'Office Location',
+    options: [
+      {
+        value: 'new york',
+        label: 'New York',
+      },
+      {
+        value: 'san fransis',
+        label: 'San Fransis',
+      },
+      {
+        value: 'dublin',
+        label: 'Dublin',
+      },
+    ],
+  },
+  {
+    value: 'role',
+    label: 'Role',
+    options: [
+      {
+        value: 'office manager',
+        label: 'Office Manager',
+      },
+      {
+        value: 'csm',
+        label: 'CSM',
+      },
+      {
+        value: 'ae',
+        label: 'AE',
+      },
+      {
+        value: 'vp of sales',
+        label: 'VP of sales',
+      },
+      {
+        value: 'ceo',
+        label: 'CEO',
+      },
+      {
+        value: 'cfo',
+        label: 'CFO',
+      },
+    ],
+  },
+];
+
+export default options;
