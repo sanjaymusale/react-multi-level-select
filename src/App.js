17018edfd5ddbbc0c4fbaad1ab3e6f9193e768c3
sanjaymusale <sanjay@codebrahma.com>
2019-10-23 11:54:39 +0530

    output displayed when options selected

--- src/App.js
+++ src/App.js
@@ -1,18 +1,40 @@
 import React from 'react';
+import ReactJson from 'react-json-view'
 import './App.css';
 import MultiSelect from './multiSelect';
 import options from './options';
-function App() {
-  return (
-    <div className="container">
-      <div className="multi-level">
-        <p className="title">React Multi Level Selector</p>
-        <MultiSelect
-          options={options}
-        />
-      </div>
-    </div>
-  );
-}
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      values: [],
+    }
+  }
 
+  onChange = (values) => {
+    this.setState({ values });
+  }
+  render() {
+    return (
+      <div className="container">
+        <div className="multi-level">
+          <p className="title">React Multi Level Selector</p>
+          <MultiSelect
+            options={options}
+            onChange={this.onChange}
+          />
+        </div>
+        <p>Output</p>
+        <div className="output">
+          <ReactJson
+            src={this.state.values}
+            enableClipboard={false}
+            displayDataTypes={false}
+            displayObjectSize={false}
+          />
+        </div>
+      </div>
+    );
+  }
+}
 export default App;
